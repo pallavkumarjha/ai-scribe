1a8b1faccc3cbd5f930844d7ff790e3e9be9c43b
pallavkumarjha <pallavkumarjha26@gmail.com>
2024-10-26 01:02:15 +0530

    Your commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // src/App.tsx
 import { ThemeProvider } from "./components/theme-provider"
 import ScribeAI from "./screens/scribeAI" // Move your main component to a separate file
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
     <ThemeProvider>
       <ScribeAI />
+      <Analytics />
     </ThemeProvider>
   )
 }
